740fb81277803eeda66701c0c7c2bb1aeef9570b
Arge1014 <nickarge1014@gmail.com>
2020-05-12 17:05:24 -0400

    webpage reacts to sensor stimulus

--- finalProject/flaskServer.py
+++ finalProject/flaskServer.py
@@ -23,28 +23,32 @@ def index():
         GPIO.setup(soundPin, GPIO.IN)
     except:
         response = "error reading sensors"
-    while noIntruders == True:
-        sound = GPIO.input(soundPin)
-        vibration = GPIO.input(vibPin)
-        motion = GPIO.input(pirPin)
-        if sound == 1:
-            print("Sound Detected")
-            soundDetected = 1
-            noIntruders = False
-        elif motion == 1:
-            print("Motion detected")
-            motionDetected = 1
-            noIntruders = False
-        elif vibration == 1:
-            print("Vibration Detected")
-            vibrationDetected = 1
-            noIntruders = False
-        time.sleep(.1)
+    with open("../log/detectionlog.csv", "a") as log:
+        while noIntruders == True:
+            sound = GPIO.input(soundPin)
+            vibration = GPIO.input(vibPin)
+            motion = GPIO.input(pirPin)
+            if sound == 1:
+                print("Sound Detected")
+                soundDetected = 1
+                noIntruders = False
+                log.write("{0}{1}\n".format(time.strftime("%Y-%m-%d %H:%M:%S"),str("Sound")))
+            elif motion == 1:
+                print("Motion detected")
+                motionDetected = 1
+                noIntruders = False
+                log.write("{0}{1}\n".format(time.strftime("%Y-%m-%d %H:%M:%S"),str("Motion")))
+            elif vibration == 1:
+                print("Vibration Detected")
+                vibrationDetected = 1
+                noIntruders = False
+                log.write("{0}{1}\n".format(time.strftime("%Y-%m-%d %H:%M:%S"),str("Vibration")))
+            time.sleep(.1)
     templateData = {
-        soundDetected,
-        motionDetected,
-        vibrationDetected
+        'soundDetected' : soundDetected,
+        'motionDetected' : motionDetected,
+        'vibrationDetected' : vibrationDetected
     }
-    return render_template('index.html')
+    return render_template('index.html', **templateData)
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=80, debug=True)
